0e72fc2e73b60c2709ffb510d770921ee85949a9
AtharvVerma-290 <atharv.verma29k@gmail.com>
2024-09-26 19:58:45 +0530

    flask frontend and predict pipeline

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 import sys
@@ -8,16 +8,56 @@ from .database import init_db
 from .components.data_ingestion import DataIngestion
 from .components.data_transformation import DataTransformation
 from .pipeline.train_pipeline import TrainPipeline
+from .pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 init_db(app)
 
-logging.info("Calling train pipeline")
-train_pipeline = TrainPipeline()
-train_pipeline.initiate_training()
+# logging.info("Calling train pipeline")
+# train_pipeline = TrainPipeline()
+# train_pipeline.initiate_training()
 
 @app.route('/')
 def index():
   return "Hello, World! The testint commit"
+
+@app.route("/predictdata", methods=['GET', 'POST'])
+def predict_datapoint():
+  if request.method == 'GET':
+    return render_template('home.html')
+  else:
+    data = CustomData(
+      baseline_value=request.form.get('baseline_value'),
+      accelerations=request.form.get('accelerations'),
+      fetal_movement=request.form.get('fetal_movement'),
+      uterine_contractions=request.form.get('uterine_contractions'),
+      light_decelerations=request.form.get('light_decelerations'),
+      severe_decelerations=request.form.get('severe_decelerations'),
+      prolongued_decelerations=request.form.get('prolongued_decelerations'),
+      abnormal_short_term_variability=request.form.get('abnormal_short_term_variability'),
+      mean_value_of_short_term_variability=request.form.get('mean_value_of_short_term_variability'),
+      percentage_of_time_with_abnormal_long_term_variability=request.form.get('percentage_of_time_with_abnormal_long_term_variability'),
+      mean_value_of_long_term_variability=request.form.get('mean_value_of_long_term_variability'),
+      histogram_width=request.form.get('histogram_width'),
+      histogram_min=request.form.get('histogram_min'),
+      histogram_max=request.form.get('histogram_max'),
+      histogram_number_of_peaks=request.form.get('histogram_number_of_peaks'),
+      histogram_number_of_zeroes=request.form.get('histogram_number_of_zeroes'),
+      histogram_mode=request.form.get('histogram_mode'),
+      histogram_mean=request.form.get('histogram_mean'),
+      histogram_median=request.form.get('histogram_median'),
+      histogram_variance=request.form.get('histogram_variance'),
+      histogram_tendency=request.form.get('histogram_tendency')
+    )
+    pred_df = data.get_data_as_data_frame()
+    print(pred_df)
+
+    predict_pipeline = PredictPipeline()
+    results = predict_pipeline.predict(pred_df)
+    print(results)
+    return render_template('home.html', results=results)
+  
+
+    
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,103 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+
+class PredictPipeline:
+  def __init__(self):
+    pass
+
+  def predict(self, features):
+    try:
+      model_path = 'artifacts/model.joblib'
+      feature_preprocessor_path = 'artifacts/feature_preprocessor.joblib'
+      model = load_object(file_path = model_path)
+      preprocessor = load_object(file_path=feature_preprocessor_path)
+      data_scaled = preprocessor.transform(features)
+      preds = model.predict(data_scaled)
+      return preds
+    
+    except Exception as e:
+      raise CustomException(e, sys)
+    
+
+class CustomData:
+  def __init__(self,
+              baseline_value: float,
+              accelerations: float,
+              fetal_movement: float,
+              uterine_contractions: float,
+              light_decelerations: float,
+              severe_decelerations: float,
+              prolongued_decelerations: float,
+              abnormal_short_term_variability: float,
+              mean_value_of_short_term_variability: float,
+              percentage_of_time_with_abnormal_long_term_variability: float,
+              mean_value_of_long_term_variability: float,
+              histogram_width: float,
+              histogram_min: float,
+              histogram_max: float,
+              histogram_number_of_peaks: float,
+              histogram_number_of_zeroes: float,
+              histogram_mode: float,
+              histogram_mean: float,
+              histogram_median: float,
+              histogram_variance: float,
+              histogram_tendency: float ):
+    self.baseline_value = baseline_value
+    self.accelerations = accelerations
+    self.fetal_movement = fetal_movement
+    self.uterine_contractions = uterine_contractions
+    self.light_decelerations = light_decelerations
+    self.severe_decelerations = severe_decelerations
+    self.prolongued_decelerations = prolongued_decelerations
+    self.abnormal_short_term_variability = abnormal_short_term_variability
+    self.mean_value_of_short_term_variability = mean_value_of_short_term_variability
+    self.percentage_of_time_with_abnormal_long_term_variability = percentage_of_time_with_abnormal_long_term_variability
+    self.mean_value_of_long_term_variability = mean_value_of_long_term_variability
+    self.histogram_width = histogram_width
+    self.histogram_min = histogram_min
+    self.histogram_max = histogram_max
+    self.histogram_number_of_peaks = histogram_number_of_peaks
+    self.histogram_number_of_zeroes = histogram_number_of_zeroes
+    self.histogram_mode = histogram_mode
+    self.histogram_mean = histogram_mean
+    self.histogram_median = histogram_median
+    self.histogram_variance = histogram_variance
+    self.histogram_tendency = histogram_tendency
+
+
+
+  def get_data_as_data_frame(self):
+    try:
+      custom_data_input_dict = {
+        "baseline_value": [self.baseline_value],
+        "accelerations": [self.accelerations],
+        "fetal_movement": [self.fetal_movement],
+        "uterine_contractions": [self.uterine_contractions],
+        "light_decelerations": [self.light_decelerations],
+        "severe_decelerations": [self.severe_decelerations],
+        "prolongued_decelerations": [self.prolongued_decelerations],
+        "abnormal_short_term_variability": [self.abnormal_short_term_variability],
+        "mean_value_of_short_term_variability": [self.mean_value_of_short_term_variability],
+        "percentage_of_time_with_abnormal_long_term_variability": [self.percentage_of_time_with_abnormal_long_term_variability],
+        "mean_value_of_long_term_variability": [self.mean_value_of_long_term_variability],
+        "histogram_width": [self.histogram_width],
+        "histogram_min": [self.histogram_min],
+        "histogram_max": [self.histogram_max],
+        "histogram_number_of_peaks": [self.histogram_number_of_peaks],
+        "histogram_number_of_zeroes": [self.histogram_number_of_zeroes],
+        "histogram_mode": [self.histogram_mode],
+        "histogram_mean": [self.histogram_mean],
+        "histogram_median": [self.histogram_median],
+        "histogram_variance": [self.histogram_variance],
+        "histogram_tendency": [self.histogram_tendency]
+      }
+
+      return pd.DataFrame(custom_data_input_dict)
+      
+    except Exception as e:
+      raise CustomException(e, sys)
+
+    
